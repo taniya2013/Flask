7c6949284dbc4256440ef5754e483630d339f47a
Taniya <tt835554@gmail.com>
2026-06-29 13:56:27 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask , render_template                
+app = Flask(__name__)  
+print(__name__)  
+@app.route("/")  
+def home_page():  
+    return render_template("home.html") 
+if (__name__=="__main__"): 
+     app.run(host="0.0.0.0", port=5000 , debug=False )  
+     #app.run(debug=False)  
+    # print("hello")  
+
+
+
+
+
+    
+    
+
+
